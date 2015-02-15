bbbe0e87b48847cb83172e1a67444fe1f8fa13d0
soIhateyou <jszhaofeng@qq.com>
2015-02-15 15:02:53 +0800

    create ch3

---
+++ CH3.LogAn/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace CH3.LogAn
+{
+    public class Calculator
+    {
+        private int _sum;
+
+        public void Add(int number)
+        {
+            _sum += number;
+        }
+
+        public int Sum()
+        {
+            int temp = _sum;
+            _sum = 0;
+            return temp;
+        }
+    }
+}
---
+++ CH3.LogAnTest/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CH3.LogAn;
+using NUnit.Framework;
+
+namespace CH3.LogAnTest
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Sum_NoAddCalls_DefaultsToZero()
+        {
+            Calculator calc = new Calculator();
+            int lastSum = calc.Sum();
+            Assert.AreEqual(0,lastSum);
+        }
+    }
+}
